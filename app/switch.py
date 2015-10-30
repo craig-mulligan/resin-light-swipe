5e3c7b0bad86efe9c0578cdca2fbfcf39f1c14ba
Craig Mulligan <craig@firingsuad.co.za>
2015-10-30 17:45:45 +0000

    add lifx

--- app/switch.py
+++ app/switch.py
@@ -1,37 +1,21 @@
 #!/usr/bin/env python
 import skywriter
 import signal
+import lifx
 
-some_value = 5000
-
-# @skywriter.move()
-# def move(x, y, z):
-#   print( x, y, z )
+# Create the LIFX connection
+lights = lifx.Lifx()
 
 @skywriter.flick()
-def flick(start,finish):
-  print('Got a flick!', start, finish)
-
-@skywriter.airwheel()
-def spinny(delta):
-  global some_value
-  some_value += delta
-  if some_value < 0:
-    some_value = 0
-  if some_value > 10000:
-    some_value = 10000
-  print('Airwheel:', some_value/100)
-
-@skywriter.double_tap()
-def doubletap(position):
-  print('Double tap!', position)
+def flick(start, finish):
+    print('Got a flick!', start, finish)
 
 @skywriter.tap()
 def tap(position):
-  print('Tap!', position)
-
-@skywriter.touch()
-def touch(position):
-  print('Touch!', position)
+    print('Tap!', position)
+    if lights.is_on() == 0:
+        lights.on()
+    else:
+        lights.off()
 
 signal.pause()
